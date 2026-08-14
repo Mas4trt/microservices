57794c8d9c75620876d1c4cedd9b99a65f106d78
Yaroslav.K <fokrivosheev@mail.ru>
2026-08-14 15:20:54 +0500

    test: check API status codes for errors

--- payment/internal/api/payment/v1/pay.go
+++ payment/internal/api/payment/v1/pay.go
@@ -2,14 +2,22 @@ package v1
 
 import (
 	"context"
+	"errors"
 
+	"github.com/Mas4trt/microservices/payment/internal/converter"
+	"github.com/Mas4trt/microservices/payment/internal/model"
 	paymentV1 "github.com/Mas4trt/microservices/shared/pkg/proto/payment/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (a *api) PayOrder(ctx context.Context, req *paymentV1.PayOrderRequest) (*paymentV1.PayOrderResponse, error) {
-	transactionUUID, err := a.paymentService.PayOrder(ctx, req.OrderUuid)
+	transactionUUID, err := a.paymentService.PayOrder(ctx, converter.PayOrderRequestToModel(req))
 	if err != nil {
-		return nil, err
+		if errors.Is(err, model.ErrInvalidPaymentMethod) {
+			return nil, status.Error(codes.InvalidArgument, "invalid payment method")
+		}
+		return nil, status.Error(codes.Internal, "internal server error")
 	}
 
 	return &paymentV1.PayOrderResponse{
