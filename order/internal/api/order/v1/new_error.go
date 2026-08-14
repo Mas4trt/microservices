6b99c375df7f80d7e1ed6cf93b100f6f2f056ce5
Yaroslav.K <fokrivosheev@mail.ru>
2026-08-14 13:35:42 +0500

    fix(order/api): log underlying error cause in NewError

--- order/internal/api/order/v1/new_error.go
+++ order/internal/api/order/v1/new_error.go
@@ -2,11 +2,14 @@ package v1
 
 import (
 	"context"
+	"log"
 
 	orderV1 "github.com/Mas4trt/microservices/shared/pkg/openapi/order/v1"
 )
 
 func (a *api) NewError(ctx context.Context, err error) *orderV1.GenericErrorStatusCode {
+	log.Printf("internal error: %v", err)
+
 	return &orderV1.GenericErrorStatusCode{
 		StatusCode: 500,
 		Response: orderV1.GenericError{
