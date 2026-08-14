7ea964faf3e7fcb8e7bbd32a05d9c565cd86f685
Yaroslav.K <fokrivosheev@mail.ru>
2026-08-14 15:21:08 +0500

    feat: validate payment method in service

--- payment/internal/service/payment/pay.go
+++ payment/internal/service/payment/pay.go
@@ -4,15 +4,20 @@ import (
 	"context"
 	"log"
 
+	"github.com/Mas4trt/microservices/payment/internal/model"
 	"github.com/google/uuid"
 )
 
-func (s *service) PayOrder(_ context.Context, orderUUID string) (string, error) {
+func (s *service) PayOrder(_ context.Context, req model.PayOrderRequest) (string, error) {
+	if req.PaymentMethod == model.PaymentMethodUnspecified {
+		return "", model.ErrInvalidPaymentMethod
+	}
+
 	transactionUUID := uuid.NewString()
 
 	log.Printf(
 		"Processed payment for order %s, transaction ID: %s",
-		orderUUID,
+		req.OrderUUID,
 		transactionUUID,
 	)
 
