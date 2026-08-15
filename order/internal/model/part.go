65d4c3c2c4645f6e3dd2168b1236982662b93c78
Yaroslav.K <fokrivosheev@mail.ru>
2026-08-15 14:39:10 +0500

    refactor(order): replace full inventory Part model with narrow PricedPart projection

--- order/internal/model/part.go
+++ order/internal/model/part.go
@@ -1,10 +1,19 @@
 package model
 
-import (
-	"time"
+import "github.com/google/uuid"
 
-	"github.com/google/uuid"
-)
+type PricedPart struct {
+	UUID  uuid.UUID
+	Price float64
+}
+
+type PartsFilter struct {
+	UUIDs                 []string
+	Names                 []string
+	Categories            []Category
+	ManufacturerCountries []string
+	Tags                  []string
+}
 
 type Category int32
 
@@ -15,61 +24,3 @@ const (
 	CategoryPorthole    Category = 3
 	CategoryWing        Category = 4
 )
-
-var categoryName = map[Category]string{
-	CategoryUnspecified: "CATEGORY_UNSPECIFIED",
-	CategoryEngine:      "CATEGORY_ENGINE",
-	CategoryFuel:        "CATEGORY_FUEL",
-	CategoryPorthole:    "CATEGORY_PORTHOLE",
-	CategoryWing:        "CATEGORY_WING",
-}
-
-func (c Category) String() string {
-	if name, ok := categoryName[c]; ok {
-		return name
-	}
-	return "CATEGORY_UNSPECIFIED"
-}
-
-type Dimensions struct {
-	Length float64
-	Width  float64
-	Height float64
-	Weight float64
-}
-
-type Manufacturer struct {
-	Name    string
-	Country string
-	Website string
-}
-
-type Value struct {
-	StringValue *string
-	Int64Value  *int64
-	DoubleValue *float64
-	BoolValue   *bool
-}
-
-type Part struct {
-	UUID          uuid.UUID
-	Name          string
-	Description   string
-	Price         float64
-	StockQuantity int64
-	Category      Category
-	Dimensions    Dimensions
-	Manufacturer  Manufacturer
-	Tags          []string
-	Metadata      map[string]any
-	CreatedAt     time.Time
-	UpdatedAt     *time.Time
-}
-
-type PartsFilter struct {
-	UUIDs                 []string
-	Names                 []string
-	Categories            []Category
-	ManufacturerCountries []string
-	Tags                  []string
-}
